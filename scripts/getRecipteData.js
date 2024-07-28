2539e5204f9dfeb90d0627940f2f0cefffbea1e6
Linus Markstrom <linusmarkstrom00@gmail.com>
2024-07-28 17:04:21 +0200

    fixed

--- scripts/getRecipteData.js
+++ scripts/getRecipteData.js
@@ -3,8 +3,7 @@ const app_key = '98bc9840d51741e2878170b56863111a'; // api nyckel
 
 // Funktion som hämtar recept från API:n genom att använda en söksträng
 export function getRecipes(search) {
-    var query = search;
-    var query = `https://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}`;
+    var query = `https://api.edamam.com/search?q=${search}&app_id=${app_id}&app_key=${app_key}`;
 
     return fetch(query)
         .then(response => {
@@ -23,17 +22,21 @@ export function getRecipes(search) {
 }
 
 // Function som hämtar detaljer för ett specifikt recept från API:n
-export async function getRecipeDetails(recipeId) {
+export function getRecipeDetails(recipeId) {
     const query = `https://api.edamam.com/api/recipes/v2/${recipeId}?type=public&app_id=${app_id}&app_key=${app_key}`;
 
-    try {
-        const response = await fetch(query);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json(); // inväntar svaret genom json
-        return data.recipe; // returnerar datan
-    } catch (error) { 
-        console.error('Error fetching recipe details:', error);
-    }
+    return fetch(query)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json(); // returnerar genom json
+        })
+        .then(data => {
+            return data.recipe; // returnerar datan
+        })
+        .catch(error => { 
+            console.error('There was a problem with the fetch operation:', error);
+            throw error; 
+        });
 }
